75779bb83e58fd0023e6d8d488e18ca46872b4f6
Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>
2020-07-10 12:01:59 +0100

    Trying more stuff

--- .github/pr-comment.js
+++ .github/pr-comment.js
@@ -1,16 +1,27 @@
 const env = process.env;
 
-const body = `
- :test_tube: **Experiment: nothing below is actually true**
+const body = [`
+:test_tube: **Experiment: nothing below is actually true**
 
 **The \`${env.WORKFLOW}\` workflow has identified this as a release preparation PR**
 
-(This is because the name of the PR branch, \`${env.HEAD_REF}\`, starts with \`prepare-\`, and the \`foo.md\` file has been modified)
+(This is because the name of the PR branch, \`${env.HEAD_REF}\` starts with \`prepare-\`, and the \`__init__.py\` file has been modified)
+`];
 
+const checklist = () => `
 **Checklist:**
-- ${env.ASDF.includes("3.0.1") ? "✔️ Version number is correct" : "⚠️ Version number appears incorrect"}
-- ${env.ASDF.includes("MEOW MEOW") ? "✔️ Something is okay" : "⚠️ Something is not okay"}
+- ${env.ASDF.includes("3.0.1") ? "✔️ Thingy number" : "⚠️ Thingy number appears incorrect"}
+- ${parseInt(env.MILESTONE_OPEN_ISSUES) > 0 ? "⚠️" : "✔️"} ${env.MILESTONE_OPEN_ISSUES} open issue(s) on [milestone ${env.MILESTONE_TITLE}](https://github.com/${env.REPOSITORY}/milestone/${env.MILESTONE_NUMBER})
 `;
 
+const onJobFail = () => `
+❌ **Something went wrong - check the workflow run**
+`;
 
-console.log(JSON.stringify({"body": body}))
+if (env.JOB_STATUS !== "success") {
+    body.push(onJobFail());
+} else {
+    body.push(checklist());
+}
+
+console.log(JSON.stringify({"body": body.join("")}));

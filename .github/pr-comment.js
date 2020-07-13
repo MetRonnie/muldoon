fdc4d5142df5b5e53d10147f922d8137bdadb0c4
Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>
2020-07-13 11:18:58 +0100

    Finalised

--- .github/pr-comment.js
+++ .github/pr-comment.js
@@ -1,27 +1,62 @@
 const env = process.env;
+const echo = console.log;
 
-const body = [`
+const returnPayload = (body) => JSON.stringify({"body": body});
+
+const versionText = () => {
+    let checkbox = "[x]";
+    let note = "";
+    if (! env.HEAD_REF.includes(env.INIT_VERSION)) {
+        checkbox = "[ ]";
+        note = "(⚠️ does not match version number in PR branch name)";
+    }
+    return `${checkbox} Version number: \`${env.INIT_VERSION}\` ${note}`
+};
+
+const milestoneText = () =>  {
+    let checkbox = "[ ]";
+    let note = `⚠️ Could not find milestone matching ${env.INIT_VERSION}`;
+    if (env.MILESTONE_JSON) {
+        const milestone = JSON.parse(env.MILESTONE_JSON);
+        if (parseInt(milestone.open_issues) === 0) {
+            checkbox = "[x]";
+        }
+        note = `${milestone.open_issues} open issues/PRs on [milestone ${milestone.title}](https://github.com/${env.REPOSITORY}/milestone/${milestone.number})`;
+    }
+    return `${checkbox} Milestone complete? ${note}`;
+};
+
+const checklistText = () => `
+**Checklist:**
+- ${versionText()}
+- ${milestoneText()}
+- [ ] Changelog up-to-date?
+- [ ] Contributors up-to-date in README?
+- [ ] \`.mailmap\` file up-to-date?
+
+setup.py bdist test: ✔️ succeeded
+`;
+
+const jobFailText = () => `
+❌ **Something went wrong - check the workflow run**
+`;
+
+const fullText = () => `
 :test_tube: **Experiment: nothing below is actually true**
 
 **The \`${env.WORKFLOW}\` workflow has identified this as a release preparation PR**
 
 (This is because the name of the PR branch, \`${env.HEAD_REF}\` starts with \`prepare-\`, and the \`__init__.py\` file has been modified)
-`];
 
-const checklist = () => `
-**Checklist:**
-- ${env.ASDF.includes("3.0.1") ? "✔️ Thingy number" : "⚠️ Thingy number appears incorrect"}
-- ${parseInt(env.MILESTONE_OPEN_ISSUES) > 0 ? "⚠️" : "✔️"} ${env.MILESTONE_OPEN_ISSUES} open issue(s) on [milestone ${env.MILESTONE_TITLE}](https://github.com/${env.REPOSITORY}/milestone/${env.MILESTONE_NUMBER})
+${env.JOB_STATUS === "success" ? checklistText() : jobFailText()}
 `;
 
-const onJobFail = () => `
-❌ **Something went wrong - check the workflow run**
-`;
-
-if (env.JOB_STATUS !== "success") {
-    body.push(onJobFail());
-} else {
-    body.push(checklist());
+let payload;
+try {
+    payload = returnPayload(fullText());
+} catch (err) {
+    payload = returnPayload(jobFailText());
+    console.error(err);
 }
 
-console.log(JSON.stringify({"body": body.join("")}));
+echo(payload);

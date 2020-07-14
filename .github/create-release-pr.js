6011bb309465621bb7815d481941d18f847e8686
Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>
2020-07-14 22:20:23 +0100

    Finalised

--- .github/create-release-pr.js
+++ .github/create-release-pr.js
@@ -1,46 +1,29 @@
 const {exec} = require('child_process');
 const env = process.env;
+const curlOpts = '--silent --fail'
 
-// const getMilestone = () => {
-//     const request = `curl https://api.github.com/repos/${env.REPOSITORY}/milestones \
-//         -H "authorization: Bearer $GH_TOKEN" \
-//         --silent --fail`;
-//         // Don't use env.GH_TOKEN above as that might print in log.
-//     exec(request, (err, stdout, stderr) => {
-//         if (err) throw err;
-//         if (stderr) throw stderr;
-//         const response_arr = JSON.parse(stdout);
-//         for (const milestone of response_arr) {
-//             if (milestone.title.includes(env.VERSION)) {
-//                 console.log(`Title: ${milestone.title}, open issues: ${milestone.open_issues}`);
-//                 return milestone;
-//             }
-//         }
-//         console.log(`::warning :: Could not find milestone matching "${env.VERSION}"`);
-//         console.log(response_arr);
-//     });
-// };
+const milestone = getMilestone();
 
-// const milestoneText = () => {
-//     const milestone = getMilestone();
-//     let checkbox = "[ ]";
-//     let note = `⚠️ Could not find milestone matching ${env.VERSION}`;
-//     if (milestone) {
-//         if (parseInt(milestone.open_issues) === 0) {
-//             checkbox = "[x]";
-//         }
-//         note = `${milestone.open_issues} open issues/PRs on [milestone ${milestone.title}](https://github.com/${env.REPOSITORY}/milestone/${milestone.number})`;
-//     }
-//     return `${checkbox} Milestone complete? ${note}`;
-// };
+const milestoneText = () => {
+    let checkbox = "[ ]";
+    let note = `⚠️ Could not find milestone matching "${env.VERSION}"`;
+    if (milestone) {
+        if (parseInt(milestone.open_issues) === 0) {
+            checkbox = "[x]";
+        }
+        note = `${milestone.open_issues} other open issues/PRs on [milestone ${milestone.title}](https://github.com/${env.REPOSITORY}/milestone/${milestone.number})`;
+    }
+    return `${checkbox} Milestone complete? ${note}`;
+};
 
 const bodyText = `
-**This PR was automatically created by the \`${env.WORKFLOW}\` workflow**
+**This PR was created by the \`${env.WORKFLOW}\` workflow, triggered by @${env.AUTHOR}**
 
 #### Tests:
 ✔️ \`setup.py\` bdist test
 
 #### Checklist:
+- ${milestoneText()}
 - [ ] Changelog up-to-date?
 - [ ] Contributors up-to-date in README?
 - [ ] \`.mailmap\` file up-to-date?
@@ -59,19 +42,20 @@ const request = `curl -X POST \
     -H "content-type: application/json" \
     --data '${payload}' \
     --fail`;
+    // Don't use env.GH_TOKEN above as that might print in log.
 
 exec(request, (err, stdout, stderr) => {
-    handleExec(request, err, stdout, stderr);
-    response = JSON.parse(stdout);
-    setMilestoneAndAssignee(response.number);
+    handleExecResult(request, err, stdout, stderr);
+    const pr = JSON.parse(stdout);
+    setMilestoneAndAssignee(pr.number);
 });
 
 
 function setMilestoneAndAssignee(prNumber) {
     // Cannot set them when creating the PR unfortunately
     const payload = JSON.stringify({
-        milestone: 1,
-        assignees: ['MetRonnie']
+        milestone: milestone ? milestone.number : undefined,
+        assignees: [env.AUTHOR]
     });
 
     const request = `curl -X PATCH \
@@ -79,14 +63,42 @@ function setMilestoneAndAssignee(prNumber) {
         -H "authorization: Bearer $GH_TOKEN" \
         -H "content-type: application/json" \
         --data '${payload}' \
-        --fail`;
+        ${curlOpts}`;
 
     exec(request, (err, stdout, stderr) => {
-        handleExec(request, err, stdout, stderr);
+        handleExecResult(request, err, stdout, stderr);
     });
 }
 
-function handleExec(cmd, err, stdout, stderr) {
+function getMilestone() {
+    const request = `curl -X GET \
+        https://api.github.com/repos/${env.REPOSITORY}/milestones \
+        -H "authorization: Bearer $GH_TOKEN" \
+        ${curlOpts}`;
+
+    let milestone = undefined;
+    exec(request, (err, stdout, stderr) => {
+        try {
+            handleExecResult(request, err, stdout, stderr);
+        } catch (err) {
+            console.log(`::warning :: Error finding milestone`);
+            console.log(err, '\n');
+            return;
+        }
+        const response = JSON.parse(stdout);
+        for (milestone of response) {
+            if (milestone.title.includes(env.VERSION)) {
+                console.log('Found milestone:', milestone.title, '\n');
+                return;
+            }
+        }
+        console.log(`::warning :: Could not find milestone matching "${env.VERSION}"`);
+        milestone = undefined;
+    });
+    return milestone;
+}
+
+function handleExecResult(cmd, err, stdout, stderr) {
     if (err) throw err;
     console.log('=====================  cmd  ======================');
     console.log(cmd);
@@ -95,6 +107,5 @@ function handleExec(cmd, err, stdout, stderr) {
         console.log(stderr);
     };
     console.log('===================== stdout =====================');
-    console.log(stdout);
-    console.log(' ');
+    console.log(stdout, '\n');
 }

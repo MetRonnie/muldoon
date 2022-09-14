056f649dd790b5112cd46ef980d7de3cf69ab484
Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>
2022-09-14 19:25:52 +0100

    Prepare blah

--- metomi/isodatetime/__init__.py
+++ metomi/isodatetime/__init__.py
@@ -17,4 +17,4 @@
 # ----------------------------------------------------------------------------
 """Python ISO 8601 date time parser and data model/manipulation utilities."""
 
-__version__ = "3.2.12"
+__version__ = "3.11.4"
